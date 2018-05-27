55ec9409490d2d3e80fb5d893d268a35b0a19e73
Rettend <36823200+Rettend@users.noreply.github.com>
2018-05-27 16:16:31 +0200

    Update preizy.py

--- preizy.py
+++ preizy.py
@@ -12,12 +12,58 @@ async def on_ready():
     await bot.change_presence(game=discord.Game(name="diep.io"))
 
 @bot.command(pass_context=True)
-async def ben(user):
-    if user == bot.user.name:
-        bot.say("I wont ben myself xd")
-    if user != bot.user.name:
-        bot.say(f"{ctx.message.author} banned {user.name}...")
+async def roll(ctx, x : int, y : int):
+    msg = random.randint(x, y)
+    text = await bot.send_message(ctx.message.channel, "**Hmmm...**")
+    await asyncio.sleep(3)
+    await bot.edit_message(text, f"**Oh, my choose: {msg}**")
 
+@bot.command(pass_context=True)
+async def sub(ctx, x : int, y : int):
+    msg = x - y
+    text = await bot.send_message(ctx.message.channel, "**Hmmm...**")
+    await asyncio.sleep(3)
+    await bot.edit_message(text, f"**Oh, the result: {msg}**")
+    
+@bot.command(pass_context=True)
+async def mul(ctx, x : int, y : int):
+    msg = x * y
+    text = await bot.send_message(ctx.message.channel, "**Hmmm...**")
+    await asyncio.sleep(3)
+    await bot.edit_message(text, f"**Oh, the result: {msg}**")
+    
+@bot.command(pass_context=True)
+async def div(ctx, x : int, y : int):
+    msg = x / y
+    text = await bot.send_message(ctx.message.channel, "**Hmmm...**")
+    await asyncio.sleep(3)
+    await bot.edit_message(text, f"**Oh, the result: {msg}**")
+    
+@bot.command(pass_context=True)
+async def exp(ctx, x : int, y : int):
+    msg = x ** y
+    text = await bot.send_message(ctx.message.channel, "**Hmmm...**")
+    await asyncio.sleep(3)
+    await bot.edit_message(text, f"**Oh, the result: {msg}**")
+    
+@bot.command(pass_context=True)
+async def add(ctx, x : int, y : int):
+    msg = x + y
+    text = await bot.send_message(ctx.message.channel, "**Hmmm...**")
+    await asyncio.sleep(3)
+    await bot.edit_message(text, f"**Oh, the result: {msg}**")
+    
+@bot.command()
+async def game(play):
+    await bot.change_presence(game=discord.Game(name=play))
+    em = discord.Embed(title="Game Status", description=f"Game status changed to __{play}__!", colour=0x95a5a6)
+    await bot.say(embed=em)
+
+@bot.command(pass_context=True)
+async def nick(ctx, name):
+    await bot.change_nickname(ctx.message.author, name)
+    em = discord.Embed(title="Nickname", description=f"{ctx.message.author}'s nick set to __{name}__!", colour0x95a5a6)
+    await bot.say(embed=em)
 
 
 token = os.environ.get('DISCORD_TOKEN')
